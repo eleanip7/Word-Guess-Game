4d10528e383fdda3c8d3cf7541649774cfba436f
Eleani Pita <eleanipita@MacBook-Pro-de-Miguel.local>
2019-03-24 18:04:19 -0400

    kinda

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ var wordGuessed = [] // word picked to match current word
 var guessesRemaining = 0; // how many tries the user has left
 var finishedGame = false; // "press key to try again"
 var wins = 0;
+var losses =0;
 
 
 //user presses any key to start the game
@@ -17,6 +18,13 @@ function startGame () {
     
 
 computerPick = Math.floor(Math.random()* (countryNames.length));
+
+function displayStats() {
+    document.getElementById("lettersGuessed").textContent = "Guessed Letters: " + lettersGuessed;
+    document.getElementById("guessesRemaining").textContent = "Guesses Remaining: " + guessesRemaining;
+    document.getElementById("wins").textContent = "Wins: " + wins;
+    document.getElementById("losses").textContent = "Losses: " + losses;
+}
 
 if(countryNames[computerPick] == countryNames[0]) {
     document.getElementById ("brazil0").src = "assets/images/brazil.png";
@@ -38,7 +46,7 @@ userGuesses = [];
 wordGuessed = [];
 
 for (var i=0; i < countryNames[computerPick].length; i++) {
-    wordGuessed.push("__");
+    wordGuessed.push(" __ ");
 }
 
 document.getElementById("tryagain").style.cssText ="display: none;"
@@ -49,38 +57,12 @@ refreshScreen();
 
 function refreshScreen () {
 
-document.getElementById('gamewins').innerText = wins;
-
-    var guessingWordText = "";
-    for (var i=0; i < wordGuessed.length; i++) {
-        guessingWordText += wordGuessed[i];
-    }
-
-    
-    document.getElementById("guessesRemaining").innerText = guessesRemaining;
-
+    document.getElementById("lettersGuessed").textContent = "Guessed Letters: " + lettersGuessed;
+    document.getElementById("guessesRemaining").textContent = "Guesses Remaining: " + guessesRemaining;
+    document.getElementById("wins").textContent = "Wins: " + wins;
+    document.getElementById("losses").textContent = "Losses: " + losses;
 
 };
 
-    
 
 
-
-
-
-
-
-
-//generate a word at random and store it in a variable
-
-//display the length of the word to the user
-
-// if the guess is correct add letter to its correct placement under current word
-
-// if the guess is incorrect then decreases guesses remaining by 1 
-
-// if the guess is incorrect then add the letter to letters guessed
-
-// if the amount of incorrect guesses is equal to 10, tell the user they lost
-
-// If all the letters in current word are entered increment wins by 1
